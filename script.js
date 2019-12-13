8926e6ca3c1cd1baf3d903d8b664f2937fa746da
jtkfrancisco <43385696+jtkfrancisco@users.noreply.github.com>
2019-12-12 20:19:06 -0500

    Interactivity alpha

---
+++ script.js
@@ -0,0 +1,20 @@
+//-------------------------------------------MAIN NAV----------------------------------------
+$(".clickBox-home").on('click', function(){
+	window.location.href = "index.html";
+})
+
+$(".clickBox-about").on('click', function(){
+	window.location.href = "about.html";
+})
+
+$(".clickBox-photos").on('click', function(){
+	window.location.href = "photos.html";
+})
+
+$(".clickBox-media").on('click', function(){
+	window.location.href = "media.html";
+})
+
+$(".clickBox-contact").on('click', function(){
+	window.location.href = "contact.html";
+})
