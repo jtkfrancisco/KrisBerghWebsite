6fa7842262557e6004040c132c93fa1d57c52180
jtkfrancisco <43385696+jtkfrancisco@users.noreply.github.com>
2019-12-26 22:27:40 -0500

    Mobile functionality

--- script.js
+++ script.js
@@ -19,6 +19,28 @@ $(".clickBox-contact").on('click', function(){
 	window.location.href = "contact.html";
 })
 
+//-------------------------------------------MOBILE NAV----------------------------------------
+$(".Mobile_clickBox-home").on('click', function(){
+	window.location.href = "index.html";
+})
+
+$(".Mobile_clickBox-about").on('click', function(){
+	window.location.href = "about.html";
+})
+
+$(".Mobile_clickBox-photos").on('click', function(){
+	window.location.href = "photos.html";
+})
+
+$(".Mobile_clickBox-media").on('click', function(){
+	window.location.href = "media.html";
+})
+
+$(".Mobile_clickBox-contact").on('click', function(){
+	window.location.href = "contact.html";
+})
+
+
 //-------------------------------------------Content----------------------------------------
 $(".clickBox-album01").on('click', function(){
 	window.location.href = "https://softlydear.bandcamp.com/album/softly-dear";
@@ -29,5 +51,37 @@ $(".clickBox-album02").on('click', function(){
 })
 
 $(".clickBox-album03").on('click', function(){
+	window.location.href = "https://soundcloud.com/doksrobotiks/sets/doks";
+})
+
+//-------------------------------------------NFC----------------------------------------
+$(".clickBox-phone").on('click', function(){
+	alert("phone number copied to clipboard");
+})
+
+$(".clickBox-email").on('click', function(){
+	alert("email copied to clipboard")
+})
+
+$(".clickBox-website").on('click', function(){
+	window.location.href = "index.html";
+})
+$(".clickBox-instagram").on('click', function(){
+	window.location.href = "https://www.instagram.com/kris.bergh";
+})
+
+$(".clickBox-facebook").on('click', function(){
+	window.location.href = "https://www.facebook.com/kristoferjbergh";
+})
+
+$(".clickBox-apple").on('click', function(){
+	window.location.href = "https://music.apple.com/us/album/doks/1081280964";
+})
+
+$(".clickBox-spotify").on('click', function(){
+	window.location.href = "https://open.spotify.com/album/485bZ1elFbM3rfhZVH8cjS";
+})
+
+$(".clickBox-soundcloud").on('click', function(){
 	window.location.href = "https://soundcloud.com/doksrobotiks/sets/doks";
 })
